3cd6feeaee73fb4e8f4dd853ce098e987b571344
Ashley <ashleynjeri7@gmail.com>
2022-10-05 18:48:37 +0300

    State & Arrays Commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,12 +6,49 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]; // addition
+    setFoods(newFoodArray); // addition
   }
 
+  // addition of handleCLick function (to remove a food item from the list upon clicking)
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+  
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  }); 
+
+  // function to update elements in array
+  /*
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+  */
+  
+  // addition of onClick to <li>
   const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine} 
     </li>
   ));
 
@@ -19,6 +56,13 @@ function SpicyFoodList() {
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+    </select>; 
     </div>
   );
 }
